685cf8d8a085719faa3618b2b61194fa1539c321
Raghav Bali <raghavbali@Raghavs-MacBook-Air.local>
2021-10-19 13:02:02 -0500

    Added a read column to the messenger_backend_messages table.

--- server/messenger_backend/views/api/messages.py
+++ server/messenger_backend/views/api/messages.py
@@ -21,18 +21,19 @@ class Messages(APIView):
             text = body.get("text")
             recipient_id = body.get("recipientId")
             sender = body.get("sender")
+            read = body.get("read")
 
             # if we already know conversation id, we can save time and just add it to message and return
             if conversation_id:
                 conversation = Conversation.objects.filter(id=conversation_id).first()
                 message = Message(
-                    senderId=sender_id, text=text, conversation=conversation
+                    senderId=sender_id, text=text, conversation=conversation, read=read
                 )
                 message.save()
                 message_json = message.to_dict()
                 return JsonResponse({"message": message_json, "sender": body["sender"]})
 
-            # if we don't have conversation id, find a conversation to m       ake sure it doesn't already exist
+            # if we don't have conversation id, find a conversation to make sure it doesn't already exist
             conversation = Conversation.find_conversation(sender_id, recipient_id)
             if not conversation:
                 # create conversation
@@ -42,7 +43,7 @@ class Messages(APIView):
                 if sender and sender["id"] in online_users:
                     sender["online"] = True
 
-            message = Message(senderId=sender_id, text=text, conversation=conversation)
+            message = Message(senderId=sender_id, text=text, conversation=conversation, read=read)
             message.save()
             message_json = message.to_dict()
             return JsonResponse({"message": message_json, "sender": sender})
