133346769e45f7246e5505d36c5b319d3ab214d0
Raghav Bali <raghavbali@Raghavs-MacBook-Air.local>
2021-10-24 18:22:29 -0500

    Updated socket logic to update read messages if the receiving user is on the sending user's conversation when a new message is sent.

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,6 +6,7 @@ import {
   addOnlineUser,
   messagesRead,
 } from "./store/conversations";
+import { updateReadMessages } from "./store/utils/thunkCreators";
 
 const socket = io(window.location.origin);
 
@@ -21,6 +22,18 @@ socket.on("connect", () => {
   });
   socket.on("new-message", (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
+    const currentState = store.getState();
+    if (data.message.conversationId === currentState.activeConversation) { //Check to see if the receiving user has the active chat set for the sending user's convo
+      for (let i = 0;i < currentState.conversations.length; i++) {
+        if (currentState.conversations[i].id === currentState.activeConversation) { //Search for the conversation to update and then run the thunk function on it
+          store.dispatch(updateReadMessages(currentState.conversations[i]));
+          break;
+        }
+        else {
+          continue;
+        }
+      }
+    }
   });
   socket.on("messages-read", (data) => {
     store.dispatch(messagesRead(data.conversation));
