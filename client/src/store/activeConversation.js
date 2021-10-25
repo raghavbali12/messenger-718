b1eaf73ddee9c3f12a40b80a28c922e83cc64aec
Raghav Bali <raghavbali@Raghavs-MacBook-Air.local>
2021-10-25 11:45:16 -0500

    Reverted changes to activeConversation to use other user's username instead of conversation id, as it may be undefined in the case of new chats.

--- client/src/store/activeConversation.js
+++ client/src/store/activeConversation.js
@@ -3,14 +3,14 @@ const SET_ACTIVE_CHAT = "SET_ACTIVE_CHAT";
 export const setActiveChat = (conversation) => {
   return {
     type: SET_ACTIVE_CHAT,
-    payload: { conversationId: conversation.id, conversation: conversation }
+    payload: { username: conversation.otherUser.username, conversation: conversation }
   };
 };
 
 const reducer = (state = "", action) => {
   switch (action.type) {
     case SET_ACTIVE_CHAT: {
-      return action.payload.conversationId; //Set the activeConversation to be the unique conversation ID
+        return action.payload.username;
     }
     default:
       return state;
