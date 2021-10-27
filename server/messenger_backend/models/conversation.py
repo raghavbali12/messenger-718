b421866d656af99575346c4ce2f0b062094f4e93
Raghav Bali <raghavbali@Raghavs-MacBook-Air.local>
2021-10-27 13:07:30 -0500

    Made changes to the backend models to prepare for implementing multiple users in a conversation.

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,28 +1,30 @@
 from django.db import models
 from django.db.models import Q
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
 
 from . import utils
 from .user import User
+from server.messenger_backend.models import user
 
 
 class Conversation(utils.CustomModel):
 
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    numberOfParticipants = models.IntegerField(validators=[MinValueValidator(2),MaxValueValidator(12)]) #ensure number of users is at least 2 and at most 12 (for now)
+    
+    
+    def validate_user_id(userId):
+        try:
+            return User.objects.get(pk=userId)
+        except User.DoesNotExist:
+            raise ValidationError(_('%(id)s is not a valid user id'),
+        params={'id': userId},
+        )
+    
+    
+    participants = ArrayField(models.IntegerField(validators=[validate_user_id])) #set participants to be a list of user ids, but validate that they exist first
+  
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
-
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
-        # return conversation or None if it doesn't exist
-        try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
-        except Conversation.DoesNotExist:
-            return None
--- server/messenger_backend/models/message.py
+++ server/messenger_backend/models/message.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
+from .user import User
 
 from . import utils
 from .conversation import Conversation
@@ -16,4 +20,23 @@ class Message(utils.CustomModel):
     )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    read = models.BooleanField(default=False)
+
+    #validate that the integer is an actual user id
+    def validate_user_id(userId):
+        try:
+            return User.objects.get(pk=userId)
+        except User.DoesNotExist:
+            raise ValidationError(_('%(id)s is not a valid user id'),
+        params={'id': userId},
+        )
+    
+    #validate that the user being added to readBy is actually part of the conversation
+    def validate_user_participant(userId, conversationId):
+        conversation = Conversation.objects.get(pk=conversationId)
+        conversationParticipants = conversation.participants
+        if userId not in conversationParticipants:
+            raise ValidationError(_('User %(id)s is not part of this conversation'),
+        params={'id': userId},
+        )
+
+    readBy = ArrayField(models.IntegerField(validators=[validate_user_id, validate_user_participant])) #keep track of all users in the conversation that have read the message
